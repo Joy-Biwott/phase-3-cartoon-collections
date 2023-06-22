84c0e0618a285b6edb9a93ea8e7ab9fd2d90e46e
Joy <biwott.joy.03@gmail.com>
2023-06-22 10:26:04 +0300

    cartoon-collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array
   # Your code here
+  array.each.with_index do |str, index|
+    puts "#{index+1}. #{str}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet planeteer_calls
   # Your code here
+  planeteer_calls.map do |planeteer_call|
+    "#{planeteer_call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls
   # Your code here
+  calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese snacks
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find { |item| cheese_types.include?(item)}
 end
